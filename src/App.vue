70c169a42250ad2b7277aa001832bd2b97fe1d73
Priana Hanif <prianahanif28@gmail.com>
2023-01-21 23:36:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data(){
+    return{
+      datasensor: "60",
+      datasensor2:"80",
+    };
+  },
+  created(){
+    //this.getData()
+  },
+  methods:{
+    async getData() {
+      const data = await  fetch("http://localhost/sensor1")
+      const datajson = await data.json()
+      this.datasensor = datajson.sensor1
+    },
+  },
+};
+</script>
+
+<template>
+  <div>Hello World {{ datasensor }}
+    <button @click="getData" class="bg-red-400 p-2 rounded-3xl">Ambil Data</button>
+  </div>
+</template>
+
